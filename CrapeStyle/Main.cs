3469555207fada907409c0e95bcadea737f7f91d
frg2089 <frg2089@qq.com>
2019-11-26 13:00:55 +0800

    做出新UI了

---
+++ CrapeStyle/Main.cs
@@ -0,0 +1,23 @@
+﻿namespace Plugin
+{
+    using CrapeStyle;
+    class Main
+    {
+        public string Name() => "Crape StudioUI库";
+        public string Version() => "1.000α";
+        public string Summary() => "这是一套UI库..应该放到Librarys文件夹中";
+        public string Inventors() => "舰队的偶像-岛风酱,小星星";
+        public string Copyright() => "Copyright ©  2019 舰队的偶像-岛风酱 , All Rights Reserved.";
+        public string[] Librarys() => new string[]{
+        };
+        public void Start()
+        {
+            var w = new Window1();
+            w.Show();
+        }
+        public string Info()
+        {
+            return "UI库";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Crape-Studio [![构建状态](https://dev.azure.com/frg2089/Crape%20Studio/_apis/build/status/frg2089.Crape-Studio?branchName=master)](https://dev.azure.com/frg2089/Crape%20Studio/_build/latest?definitionId=1&branchName=master)
 一个为RA2MOD设计的专用的工具集  
+
+# 定制UI做出来辣!  
+
+
 [Crape Studio 项目](https://github.com/frg2089/Crape-Studio/)许可证:![项目许可证](https://www.gnu.org/graphics/gplv3-88x31.png)  
 [Crape Studio 插件](https://github.com/frg2089/Crape-Studio-Plugin/)许可证:![插件许可证](https://www.gnu.org/graphics/lgplv3-88x31.png)  
 注意: 插件许可证仅对本项目库中的插件有效  
